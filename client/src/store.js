a54a4ea9aa8fde11c2f3aa4ed5c9d8b410c3db41
hemant-garg <hemant.gr6@gmail.com>
2019-02-19 21:59:16 +0530

    basic application completed

--- client/src/store.js
+++ client/src/store.js
@@ -4,14 +4,18 @@ import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
 const middleware = [thunk];
-const store = createStore(
-	rootReducer,
-	{},
-	applyMiddleware(...middleware)
-	// compose(
-	// 	applyMiddleware(...middleware),
-	// 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	// )
-);
+if (process.env.NODE_ENV === "production") {
+	const store = createStore(rootReducer, {}, applyMiddleware(...middleware));
+} else {
+	const store = createStore(
+		rootReducer,
+		{},
+		compose(
+			applyMiddleware(...middleware),
+			window.__REDUX_DEVTOOLS_EXTENSION__ &&
+				window.__REDUX_DEVTOOLS_EXTENSION__()
+		)
+	);
+}
 
 export default store;
